6f0ec463b9cbb8c098765541b9fddf5779fe2073
Saidul Islam <122171531+saidulcoder@users.noreply.github.com>
2023-02-12 17:00:09 +0600

    After adding 5 warning done

--- js/app.js
+++ js/app.js
@@ -1,20 +1,57 @@
-
-function buttonDisabled (btnID){
-    const btndisable = document.getElementById(btnID);
-    btndisable.setAttribute('disabled')
-}
-
-
-document.getElementById('btn-messi').addEventListener('click', function(x){
-    const playerName = document.getElementById('messi-name');
+function selectPlayer(playerId) {
+    const playerName = document.getElementById(playerId);
 
 
     const selectedPlayers = document.getElementById('selected-players');
 
-    const li = document.createElement ('li')
+    const li = document.createElement('li')
 
     li.innerText = playerName.innerText;
 
-    selectedPlayers.appendChild(li);
+    const selectedPlayersList = document.querySelectorAll('li')
+    if(selectedPlayersList.length>4){
+        alert('5 Player already selected');
+        return
+    }
+    else{
+        selectedPlayers.appendChild(li);
+    }
+
+}
+
+
+// Messi
+document.getElementById('btn-messi').addEventListener('click', function (x) {
+    selectPlayer('messi-name');
     x.target.disabled = true;
 })
+
+// Naymer
+document.getElementById('btn-neymar').addEventListener('click', function (x) {
+    selectPlayer('neymar-name');
+    x.target.disabled = true;
+})
+// Mbapee
+document.getElementById('btn-mbappe').addEventListener('click', function (x) {
+    selectPlayer('mbappe-name');
+    x.target.disabled = true;
+})
+// Machado
+document.getElementById('bnt-machado').addEventListener('click', function (x) {
+    selectPlayer('machado-name');
+    x.target.disabled = true;
+})
+// Ramos
+document.getElementById('btn-ramos').addEventListener('click', function (x) {
+    selectPlayer('ramos-name');
+    x.target.disabled = true;
+})
+// Renato
+document.getElementById('btn-renato').addEventListener('click', function (x) {
+    selectPlayer('renato-name');
+    x.target.disabled = true;
+})
+
+const playersSelected = document.getElementById('selected-players');
+
+console.log(playersSelected);
